23b228268e88fa9b8850a9d235dc8e51ae73db0e
Naru Neeraja <023neeraja@gmail.com>
2024-10-20 18:24:56 +0530

    Changed backend end points with deployment backend end points

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,10 +5,10 @@
 
 
 
-export const USER_API_END_POINT="https://job-backend-cl0r.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://job-backend-cl0r.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-backend-cl0r.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-backend-cl0r.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://job-backend-brown.vercel.app/api/v1/user";
+export const JOB_API_END_POINT="https://job-backend-brown.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-backend-brown.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-backend-brown.vercel.app/api/v1/company";
 
 
 
